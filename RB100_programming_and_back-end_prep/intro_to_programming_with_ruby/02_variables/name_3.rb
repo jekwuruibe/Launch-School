bcda1cf397294d21061f28df4cfb09708b3be1e3
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-06-30 01:26:19 +0100

    Submit solution to exercise 4 of Variables from Intro text

---
+++ RB100_programming_and_back-end_prep/intro_to_programming_with_ruby/02_variables/name_3.rb
@@ -0,0 +1,6 @@
+puts "Please may I know your first name? :"
+first_name = gets.chomp
+puts "Please may I know your last name? :"
+last_name = gets.chomp
+
+puts "Nice to meet you, #{first_name} #{last_name}!"
