8a0b422a680bf4238ab2cae11145585b63b5fc91
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-03 18:17:26 +0100

    Submit solution to exercise 3 of Exercises from Intro text

---
+++ RB100_programming_and_back-end_prep/intro_to_programming_with_ruby/09_exercises/exercise_03.rb
@@ -0,0 +1,8 @@
+# Now, using the same array from #2, use the select method to extract all odd numbers into a new array
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd_numbers = arr.select do |number|
+                number.odd?
+              end
+
+p odd_numbers
