3496cf36bdd385ff33a75f503c8cbd52542fc0bc
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-03 18:12:33 +0100

    Submit solution to exercise 2 of Exercises from Intro text

---
+++ RB100_programming_and_back-end_prep/intro_to_programming_with_ruby/09_exercises/exercise_02.rb
@@ -0,0 +1,7 @@
+# Use the each method of Array to iterate over [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
+# but only print out values greater than 5.
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr.each do |number|
+  puts number if number > 5
+end
