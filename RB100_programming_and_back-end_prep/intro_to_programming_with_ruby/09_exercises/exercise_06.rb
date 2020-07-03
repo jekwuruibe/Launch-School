3545c81b663b968b6c4a7cf394216c3d1903e95d
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-03 18:29:16 +0100

    Submit solution to exercise 6 of Exercises from Intro text

---
+++ RB100_programming_and_back-end_prep/intro_to_programming_with_ruby/09_exercises/exercise_06.rb
@@ -0,0 +1,9 @@
+# Get rid of duplicates without specifically removing any one value.
+
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3]
+p arr.uniq
+
+# Alternatively:
+
+arr.uniq!   # Modifies arr
+p arr
