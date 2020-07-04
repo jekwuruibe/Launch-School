e18a58f7225095286f59c1158f97c04926716a2b
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-04 01:07:24 +0100

    Submit solution to exercise 14 of Exercises from Intro text

---
+++ RB100_programming_and_back-end_prep/intro_to_programming_with_ruby/09_exercises/exercise_14.rb
@@ -0,0 +1,25 @@
+=begin
+Take the following array:
+
+a = ['white snow', 'winter wonderland', 'melting ice','slippery sidewalk', 'salted roads', 'white trees']
+
+and turn it into a new array that consists of strings containing one word. 
+(ex. ["white snow", etc...] → ["white", "snow", etc...]. 
+Look into using Array's map and flatten methods, as well as String's split method.
+=end
+
+
+
+a = ['white snow', 'winter wonderland', 'melting ice','slippery sidewalk', 'salted roads', 'white trees']
+
+new_array = a.join(" ").split(" ")
+p new_array
+
+# Alternatively:
+
+new_a = a.map do |item|
+  item.split(" ")
+end
+
+new_a = new_a.flatten
+p new_a
