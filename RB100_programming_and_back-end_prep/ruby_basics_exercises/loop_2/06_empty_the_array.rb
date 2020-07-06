15fb763e8c200b8f0ec80f635f62a2b55e202d9e
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 13:33:07 +0100

    Submit solution to exercise 6 of Loops 2 from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/loop_2/06_empty_the_array.rb
@@ -0,0 +1,15 @@
+=begin
+Empty the Array
+Given the array below, use loop to remove and print each name. 
+Stop the loop once names doesn't contain any more elements.
+
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+Keep in mind to only use loop, not while, until, etc.
+=end
+
+names = ["Sally", "Joe", "Lisa", "Henry"]
+
+loop do
+  puts names.pop
+  break if names.empty?
+end
