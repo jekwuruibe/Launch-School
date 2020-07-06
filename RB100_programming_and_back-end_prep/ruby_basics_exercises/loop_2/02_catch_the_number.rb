3574b21ee3ce481014fc6101910fa7aae64613ab
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 13:28:22 +0100

    Submit solution to exercise 2 of Loops 2 from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/loop_2/02_catch_the_number.rb
@@ -0,0 +1,16 @@
+=begin
+Catch the Number
+Modify the following code so that the loop stops if number is equal to or between 0 and 10.
+
+loop do
+  number = rand(100)
+  puts number
+end
+
+=end
+
+loop do
+  number = rand(100)
+  puts number
+  break if number >= 0 && number <= 10 
+end
