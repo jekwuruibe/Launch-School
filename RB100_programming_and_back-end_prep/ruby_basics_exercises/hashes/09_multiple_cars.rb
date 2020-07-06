0e5ae7480112f85202736e08767f9d8f540ec58d
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 23:30:47 +0100

    Submit solution to exercise 9 of Hashes from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/hashes/09_multiple_cars.rb
@@ -0,0 +1,18 @@
+=begin
+Multiple Cars
+Create a nested hash using the following data.
+
+Car
+
+type	color	year
+sedan	blue	2003
+Truck
+
+type	 color	year
+pickup	red	  1998
+=end
+
+vehicles = {
+  car:   {type: "sedan", color: "blue", year: 2000},
+  truck: {type: "pickup", color; "red", year: 1998}
+}
