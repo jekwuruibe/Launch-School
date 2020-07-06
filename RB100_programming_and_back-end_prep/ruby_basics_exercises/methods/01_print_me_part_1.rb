1706e578db5541a15ad30bee615cbfeb28625753
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 14:34:57 +0100

    Submit solution to exercise 1 of Methods from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/methods/01_print_me_part_1.rb
@@ -0,0 +1,12 @@
+=begin
+Print Me (Part 1)
+Write a method named print_me that prints "I'm printing within the method!" when invoked.
+
+print_me
+=end
+
+def print_me
+  puts "I'm printing within the method!"
+end
+
+print_me
