9a77a79940e3f2a72de2f834afa29a77d46a8708
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 22:46:35 +0100

    Submit solution to exercise 4 of Strings from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/strings/04_dynamic_string.rb
@@ -0,0 +1,14 @@
+=begin
+Dynamic String
+Modify the following code so that the value of name is printed within "Hello, !".
+
+name = 'Elizabeth'
+
+puts "Hello, !"
+Expected output:
+
+Hello, Elizabeth!
+=end
+
+name = "Elizabeth"
+puts "Hello, #{name}!"
