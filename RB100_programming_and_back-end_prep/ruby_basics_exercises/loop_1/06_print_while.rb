c416c447889552932f3a56adf924284aede17c43
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 13:04:57 +0100

    Submit solution to exercise 6 of Loops 1 from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/loop_1/06_print_while.rb
@@ -0,0 +1,27 @@
+=begin
+Print While
+Using a while loop, print 5 random numbers between 0 and 99. 
+The code below shows an example of how to begin solving this exercise.
+
+numbers = []
+
+while <condition>
+  # ...
+end
+Example output (your numbers will most likely be different):
+
+62
+96
+31
+16
+36
+
+=end
+
+numbers = (0..99).to_a   
+index = 0  
+
+while index < 5
+  puts numbers.shuffle[index]  
+  index += 1
+end
