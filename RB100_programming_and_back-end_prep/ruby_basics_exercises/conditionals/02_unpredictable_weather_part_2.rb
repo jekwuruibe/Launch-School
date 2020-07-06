9f8802e9f9ebeba7a1c9056c06a0277ea1de6906
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 22:20:35 +0100

    Submit solution to exercise 2 of Conditionals from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/conditionals/02_unpredictable_weather_part_2.rb
@@ -0,0 +1,13 @@
+=begin
+Unpredictable Weather (Part 2)
+In the code below, sun is randomly assigned as 'visible' or 'hidden'.
+
+sun = ['visible', 'hidden'].sample
+Write an unless statement that prints "The clouds are blocking the sun!" unless sun equals 'visible'.
+=end
+
+sun = ["visible", "hidden"].sample
+
+unless sun == "visible"
+  puts "The clouds are blocking the sun!"
+end
