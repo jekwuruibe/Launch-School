9d0710b602fee93728346189a24196c0cca741c4
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 23:31:22 +0100

    Submit solution to exercise 10 of Hashes from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/hashes/10_which_collection.rb
@@ -0,0 +1,16 @@
+=begin
+Which Collection?
+Rewrite car as a nested array containing the same key-value pairs.
+
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:  2003
+}
+=end
+
+car = [
+  [:type, "sedan"],
+  [:color, "blue"],
+  [:year, 2003]
+]
