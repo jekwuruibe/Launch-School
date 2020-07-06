925a6317485b4690b2d0a124c70deaaa74a6652b
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 13:31:48 +0100

    Submit solution to exercise 5 of Loops 2 from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/loop_2/05_insert_numbers.rb
@@ -0,0 +1,26 @@
+=begin
+Insert Numbers
+Modify the code below so that the user's input gets added to the numbers array. 
+Stop the loop when the array contains 5 numbers.
+
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+end
+puts numbers
+
+=end
+
+numbers = []
+count = 1
+
+loop do
+  puts "Enter any number:"
+  input = gets.chomp.to_i
+  numbers << input
+  count += 1
+  break if count > 5
+end
+puts numbers
