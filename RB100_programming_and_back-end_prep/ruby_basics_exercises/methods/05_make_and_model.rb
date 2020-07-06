d04f4b055632f42308ff2dd73004da0948d5e91f
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 14:39:49 +0100

    Submit solution to exercise 5 of Methods from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/methods/05_make_and_model.rb
@@ -0,0 +1,16 @@
+=begin
+Make and Model
+Using the following code, write a method called car that takes two arguments and prints a string 
+containing the values of both arguments.
+
+car('Toyota', 'Corolla')
+Expected output:
+
+Toyota Corolla
+=end
+
+def car(make, model)
+  puts "#{make} #{model}"
+end
+
+car("Toyota", "Corolla")
