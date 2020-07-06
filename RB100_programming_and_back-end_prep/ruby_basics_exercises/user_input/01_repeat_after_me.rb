574d0d94164beeef8894054c7352542daecc4323
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 14:11:09 +0100

    Submit solution to exercise 1 of User Input from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/user_input/01_repeat_after_me.rb
@@ -0,0 +1,15 @@
+=begin
+Repeat after me
+Write a program that asks the user to type something in, after which your program should simply display what was entered.
+
+Example:
+
+$ ruby repeater.rb
+>> Type anything you want:
+This is what I typed.
+This is what I typed.
+=end
+
+puts "Please type something:"
+user_input = gets.chomp
+puts user_input
