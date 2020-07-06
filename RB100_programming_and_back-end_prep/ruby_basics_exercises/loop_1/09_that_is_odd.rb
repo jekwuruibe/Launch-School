c01bb75d55066dc7973dfe583f3582b2618eb883
Jesse Ekwuruibe <jesseekwuruibe@gmail.com>
2020-07-06 13:11:43 +0100

    Submit solution to exercise 9 of Loops 1 from Ruby Basics

---
+++ RB100_programming_and_back-end_prep/ruby_basics_exercises/loop_1/09_that_is_odd.rb
@@ -0,0 +1,14 @@
+=begin
+That's Odd
+The code below shows an example of a for loop. 
+Modify the code so that it only outputs i if i is an odd number.
+
+for i in 1..100
+  puts i
+end
+
+=end
+
+for i in 1..100
+  puts i if i % 2 == 1
+end
